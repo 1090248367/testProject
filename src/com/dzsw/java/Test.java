1cd450fabc8c8ad5f2eec1a1ce63a230f95ada3f
1090248367 <1090248367@qq.com>
2019-04-09 14:19:45 +0800

    commit

---
+++ src/com/dzsw/java/Test.java
@@ -0,0 +1,9 @@
+package com.dzsw.java;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = "aaaaa";
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
